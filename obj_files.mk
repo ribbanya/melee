c2eed60b5552b36b9ca837c91739ddae044e6587
Robin Avery <robin@ribbanya.dev>
2024-03-05 02:02:04 -0500

    wip

--- obj_files.mk
+++ obj_files.mk
@@ -849,7 +849,6 @@ TEXT_O_FILES +=\
     $(BUILD_DIR)/asm/dolphin/dvd/dvdlow.s.o\
     $(BUILD_DIR)/src/dolphin/dvd/dvdfs.c.o\
     $(BUILD_DIR)/src/dolphin/dvd/dvd.c.o\
-    $(BUILD_DIR)/asm/dolphin/dvd/dvd.s.o\
     $(BUILD_DIR)/src/dolphin/dvd/dvdqueue.c.o\
     $(BUILD_DIR)/asm/dolphin/dvd/dvderror.s.o\
     $(BUILD_DIR)/src/dolphin/dvd/fstload.c.o\
