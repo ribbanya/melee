fe7254f9094ec4186fd6461b641a5148b8f7a7d7
Robin Avery <robin@ribbanya.dev>
2026-07-26 16:22:50 -0400

    WIP

--- src/MSL/stddef.h
+++ src/MSL/stddef.h
@@ -10,9 +10,6 @@ typedef unsigned long size_t;
 /// @todo Rename to @c size_t when #size_t is deleted.
 typedef unsigned int usize_t;
 
-typedef signed int intptr_t;
-typedef unsigned int uintptr_t;
-
 #define offsetof(type, member) ((size_t) &(((type*) 0)->member))
 
 #ifndef NULL
---
+++ src/MSL/stdint.h
@@ -0,0 +1,7 @@
+#ifndef __STDINT_H__
+#define __STDINT_H__
+
+typedef signed int intptr_t;
+typedef unsigned int uintptr_t;
+
+#endif
