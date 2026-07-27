000f0ec0991efdf06334cc9bbe83bab947c69293
Robin Avery <robin@ribbanya.dev>
2026-07-26 20:37:41 -0400

    WIP

--- src/melee/ft/chara/ftCommon/ftCo_DownAttack.h
+++ src/melee/ft/chara/ftCommon/ftCo_DownAttack.h
@@ -1,11 +1,11 @@
 #ifndef GALE01_09805C
 #define GALE01_09805C
 
+#include <stdbool.h>
+
 #include "ft/forward.h"
 #include "ftCommon/forward.h"
 #include "it/forward.h"
-
-#include <stdbool.h>
 
 /* 097270 */ void ftCo_800978D4(Fighter_GObj* gobj);
 /* 09813C */ void ftCo_DownAttack_Anim(Fighter_GObj* gobj);
