c8ad4891c4249a9ac82c0bc49e505d49a568cf0d
Ryan Burns <rburns@anduril.com>
2026-08-12 09:21:10 -0700

    Always use stdint/stdbool types
    These don't seem to need to be guarded by TARGET_PC,
    since they are standard C.

--- include/dolphin/types.h
+++ include/dolphin/types.h
@@ -7,7 +7,6 @@
 #define BIT_64 0
 #endif
 
-#ifdef TARGET_PC
 #include <stdint.h>
 typedef int8_t s8;
 typedef int16_t s16;
@@ -17,16 +16,6 @@ typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
-#else
-typedef signed char s8;
-typedef signed short int s16;
-typedef signed long s32;
-typedef signed long long int s64;
-typedef unsigned char u8;
-typedef unsigned short int u16;
-typedef unsigned long u32;
-typedef unsigned long long int u64;
-#endif
 
 typedef volatile u8 vu8;
 typedef volatile u16 vu16;
@@ -46,7 +35,6 @@ typedef volatile f64 vf64;
 
 typedef char *Ptr;
 
-#if defined(TARGET_PC)
 #include <stdbool.h>
 typedef int BOOL;
 #ifndef FALSE
@@ -54,7 +42,6 @@ typedef int BOOL;
 #endif
 #ifndef TRUE
 #define TRUE 1
-#endif
 #endif
 
 #ifdef TARGET_PC
