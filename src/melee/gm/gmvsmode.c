95fd7056755e18708e5311a8b726f56add3a23c4
Robin Avery <robin@ribbanya.dev>
2026-09-05 22:46:11 -0400

    WIP

--- src/melee/gm/gmvsmode.c
+++ src/melee/gm/gmvsmode.c
@@ -167,11 +167,11 @@ void onEnterDebugVs(GameModeState* state)
     start->rules.stkind = St_Kind_Battle;
     start->rules.xB = -1;
     start->rules.xC = -1;
-    start->rules.match_kind = MatchKind_Time;
-    start->rules.time_limit = 1;
+    start->rules.match_kind = MatchKind_Stock;
+    start->rules.time_limit = 10;
     start->rules.is_teams = true;
     start->rules.friendly_fire = true;
-    start->rules.on_unpause_override = gm_80165290;
+    // start->rules.on_unpause_override = gm_80165290;
 
     for (i = 0; i < Gm_Player_NumMax; i++) {
         gm_SetupPlayerDefaults(&start->players[i]);
