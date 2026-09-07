f9190682da17f310f85e2223eca2f9e51b90a7ae
Robin Avery <robin@ribbanya.dev>
2026-09-07 00:05:49 -0400

    WIP

--- src/fray/replaytext.c
+++ src/fray/replaytext.c
@@ -1,15 +1,19 @@
+#include "melee/gm/forward.h"
 #include "melee/if/textdraw.h"
 #include "melee/if/textlib.h"
 #include "melee/if/types.h"
 #include "melee/pl/player.h"
 
+#define TEXT_WIDTH 60
+#define TEXT_HEIGHT 7
+
 static DevText* text;
-static char buf[0x400];
+static char buf[TEXT_WIDTH * TEXT_HEIGHT];
 
 void ReplayText_Setup(void)
 {
     HSD_GObj* gobj = DevText_GetGObj();
-    text = DevText_Create(1, 20, 20, 60, 7, buf);
+    text = DevText_Create(1, 5, 5, TEXT_WIDTH, TEXT_HEIGHT, buf);
     if (text) {
         GXColor bg = { 0x00, 0x00, 0x00, 0x00 };
         GXColor fg = { 0xFF, 0xFF, 0xFF, 0xFF };
@@ -17,20 +21,20 @@ void ReplayText_Setup(void)
         DevText_HideCursor(text);
         DevText_SetBGColor(text, bg);
         DevText_SetTextColor(text, fg);
-        DevText_SetScale(text, 12.0F, 12.0F);
+        DevText_SetScale(text, 18.0f, 24.0f);
     }
 }
 
 void ReplayText_Update(void)
 {
     StaticPlayer* player;
-    s32 slot;
+    ssize_t i;
 
     DevText_Erase(text);
     DevText_SetCursorXY(text, 0, 0);
     DevText_Printf(text, "A B  C D E    F    G");
-    for (slot = 0; slot < 6; slot++) {
-        player = Player_GetPtrForSlot(slot);
+    for (i = 0; i < Gm_Player_NumMax; i++) {
+        player = Player_GetPtrForSlot(i);
         DevText_Printf(text, "\n%d %d %2d %d %2.2f %2.2f %2.2f",
                        player->player_state, player->cpu_level,
                        player->cpu_type, player->handicap, player->unk50,
