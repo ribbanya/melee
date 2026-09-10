0ea16004d3908093060c10b1382167e3d7708418
Robin Avery <robin@ribbanya.dev>
2026-09-10 03:26:59 -0400

    WIP

--- src/fray/rp/replay.c
+++ src/fray/rp/replay.c
@@ -97,7 +97,6 @@ static void recordProc(HSD_GObj* gobj)
             if (fp != NULL) {
                 OSReport("\n=== %d[%d] ===\n", i, j);
                 REPORT_INT(is_cpu = ftCo_IsCpuControlled(fp));
-                REPORT_INT(fp->cpu.is_follower);
                 REPORT_ADDR(fp);
                 if (is_cpu) {
                     REPORT_HEX(fp->cpu.buttons);
