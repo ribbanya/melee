d180c75691e8953bb77bdec19759cd42ef4f0796
Robin Avery <robin@ribbanya.dev>
2026-09-11 20:21:46 -0400

    WIP

--- src/fray/bs/bshash.c
+++ src/fray/bs/bshash.c
@@ -7,7 +7,12 @@
 #define FNV_OFFSET 0x811C9DC5U
 #define FNV_PRIME 0x01000193U
 
-u32 bsHash_HashU32(u32 h, u32 v)
+u32 bsHash_Init(void)
+{
+    return FNV_OFFSET;
+}
+
+u32 bsHash_U32(u32 h, u32 v)
 {
     h = (h ^ ((v >> 24) & 0xFF)) * FNV_PRIME;
     h = (h ^ ((v >> 16) & 0xFF)) * FNV_PRIME;
@@ -16,12 +21,12 @@ u32 bsHash_HashU32(u32 h, u32 v)
     return h;
 }
 
-u32 bsHash_HashBool(u32 h, bool b)
+u32 bsHash_Bool(u32 h, bool b)
 {
-    return bsHash_HashU32(h, b != 0);
+    return bsHash_U32(h, b != 0);
 }
 
-u32 bsHash_BitwiseF32(f32 f)
+u32 bsHash_F32Bits(f32 f)
 {
 #if defined(__MWERKS__)
     return *(u32*) &f;
@@ -32,9 +37,9 @@ u32 bsHash_BitwiseF32(f32 f)
 #endif
 }
 
-u32 bsHash_HashF32(u32 h, f32 f)
+u32 bsHash_F32(u32 h, f32 f)
 {
-    return bsHash_HashU32(h, bsHash_BitwiseF32(f));
+    return bsHash_U32(h, bsHash_F32Bits(f));
 }
 
 void bsHash_WriteU32BE(u8* dst, u32 v)
--- src/fray/bs/bshash.h
+++ src/fray/bs/bshash.h
@@ -3,10 +3,11 @@
 
 #include <Runtime/platform.h>
 
-u32 bsHash_HashU32(u32 h, u32 v);
-u32 bsHash_HashBool(u32 h, int b);
-u32 bsHash_BitwiseF32(f32 f);
-u32 bsHash_HashF32(u32 h, f32 f);
+u32 bsHash_Init(void);
+u32 bsHash_U32(u32 h, u32 v);
+u32 bsHash_Bool(u32 h, int b);
+u32 bsHash_F32Bits(f32 f);
+u32 bsHash_F32(u32 h, f32 f);
 void bsHash_WriteU32BE(u8* dst, u32 v);
 u32 bsHash_ReadU32BE(const u8* src);
 
