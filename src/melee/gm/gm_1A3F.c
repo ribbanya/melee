2f5e3c5573bd67397ef9f43505d6eb3180b124f0
Robin Avery <robin@ribbanya.dev>
2026-09-16 06:46:14 -0400

    WIP

--- src/melee/gm/gm_1A3F.c
+++ src/melee/gm/gm_1A3F.c
@@ -5,7 +5,9 @@
 #include "gmmain_lib.h"
 #include "gmscdata.h"
 #include "gmscene.h"
+#include "melee/db/db.h"
 #include "types.h"
+#include <dolphin/vi/vifuncs.h>
 #include <melee/lb/lb_00B0.h>
 #include <melee/lb/lbaudio_ax.h>
 #include <melee/lb/lbcardgame.h>
@@ -372,6 +374,14 @@ void gm_801A4510(void)
     }
 #ifdef FRAY
     state_machine.routing.curr_mode = GM_REPLAY;
+#else
+    if (VIGetDTVStatus() != 0 &&
+        (db_gameLaunchButtonState & HSD_PAD_B || OSGetProgressiveMode() == 1))
+    {
+        state_machine.routing.curr_mode = GM_PROGRESSIVE_SCAN;
+    } else {
+        state_machine.routing.curr_mode = GM_BOOT;
+    }
 #endif
     state_machine.routing.prev_mode = GM_COUNT;
 
