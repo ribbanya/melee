ce8db71011ff3e43d60db59b25da2fb301b023f4
Robin Avery <robin@ribbanya.dev>
2026-09-17 03:25:56 -0400

    WIP

---
+++ src/sysdolphin/baselib/crypt.h
@@ -0,0 +1,9 @@
+#ifndef SYSDOLPHIN_BASELIB_CRYPT_H
+#define SYSDOLPHIN_BASELIB_CRYPT_H
+
+#include <placeholder.h>
+
+int hsd_803B2FA0(u8* data, int len);
+int hsd_803B31CC(u8* data, int len);
+
+#endif
